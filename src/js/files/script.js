59b88c64cee5e8d9047e93c978125e5b897177fa
Oleksandr Shumylo <ashumylo84@gmail.com>
2023-03-10 11:04:56 +0200

    section services added

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,6 +2,7 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
 const header = document.getElementById('header');
 
 window.addEventListener('scroll', function() {
