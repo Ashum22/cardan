c0a252019f223eef0efe1b6570d1c3ec2d37404b
Oleksandr Shumylo <ashumylo84@gmail.com>
2023-08-14 11:22:37 +0300

    webpack bug fix

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,6 +6,11 @@ import { flsModules } from "./modules.js";
 const header = document.getElementById('header');
 const dropList = document.querySelectorAll('.header-dropdown__list');
 
+console.log(dropList);
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+});
+  
 window.addEventListener('scroll', function() {
     if(window.scrollY > 200) {
         changeBackgroundOnScroll()
@@ -14,6 +19,6 @@ window.addEventListener('scroll', function() {
 
 function changeBackgroundOnScroll () {
     header.classList.add('dark-background')
-    dropList.classList.add('dark-background')
+    // dropList.classList.add('dark-background')
 };
 
